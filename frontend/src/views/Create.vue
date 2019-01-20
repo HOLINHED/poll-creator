f1a0531c400b420e8d01430dff45ba7882db622d
Dawid Layne <klatkiewiczdawid@gmail.com>
2019-01-20 17:52:35 -0500

    Added create page

--- frontend/src/views/Create.vue
+++ frontend/src/views/Create.vue
@@ -1,19 +1,57 @@
 <template>
    <div id="create">
       <h1>New poll</h1>
-      <form id="poll">
+      <form id="poll" @submit.prevent="create()">
          <label for="title">Title</label>
-         <input name="title" id="title" type="text">
+         <input name="title" id="title" type="text" required>
+
+         <div id="options">
+            <div>
+               <label for="optionOne">Option 1</label>
+               <input name="optionOne" id="optionOne" type="text" required>
+            </div>
+
+            <div>
+               <label for="optionTwo">Option 2</label>
+               <input name="optionTwo" id="optionTwo" type="text" required>
+            </div>
+
+            <div>
+               <label for="optionThree">Option 3</label>
+               <input name="optionThree" id="optionThree" type="text">
+            </div>
+
+            <div>
+               <label for="optionFour">Option 4</label>
+               <input name="optionFour" id="optionFour" type="text">
+            </div>
+         </div>
+         <div id="buttons">
+            <button type="submit">Create</button>
+            <button type="reset">Reset</button>
+         </div>
       </form>
    </div>
 </template>
 
 <script>
+import store from '@/store.js';
+
 export default {
 
 };
 </script>
 
-<style>
+<style scoped>
+#options div {
+   margin-top: 30px;
+}
 
+#buttons {
+   margin-top: 20px;
+}
+
+label {
+   margin-right: 5px;
+}
 </style>
